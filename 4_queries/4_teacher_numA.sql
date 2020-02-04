1e6a204ce6373980bd9b93123c1553df517b0f50
Olivia <oliviapower.02@gmail.com>
2020-02-04 21:28:43 +0000

    4_queries

---
+++ 4_queries/4_teacher_numA.sql
@@ -0,0 +1,8 @@
+SELECT teachers.name AS teacher, cohorts.name as cohort, COUNT(assistance_requests.teacher_id) as total_assistances
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
+GROUP BY teachers.name, cohorts.name
+ORDER BY teachers.name;
